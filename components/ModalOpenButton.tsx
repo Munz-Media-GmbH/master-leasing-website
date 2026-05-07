ccc33bc77d25184a3e202ad2f997c42df46c731f
mr-patrick-s <patrick.s@muenzmedia.de>
2026-05-07 14:03:10 +0200

    feat: add ModalOpenButton client component for use in server components

---
+++ components/ModalOpenButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useContactModal } from "@/context/ContactModalContext";
+
+interface Props {
+  className?: string;
+  style?: React.CSSProperties;
+  children: React.ReactNode;
+}
+
+export default function ModalOpenButton({ className, style, children }: Props) {
+  const { openModal } = useContactModal();
+  return (
+    <button
+      type="button"
+      onClick={() => openModal()}
+      className={className}
+      style={{ border: "none", cursor: "pointer", background: "transparent", padding: 0, ...style }}
+    >
+      {children}
+    </button>
+  );
+}
